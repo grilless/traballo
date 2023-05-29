bad82cc4cbca1cfe55a89d61b0d55c40da6ed27d
roberto <robergrille10@gmail.com>
2023-05-29 12:31:17 +0200

    traballo v2

---
+++ vsftp/public/index.search.js
@@ -0,0 +1,44 @@
+var relearn_search_index = [
+  {
+    "content": "   vsFTP ou (Very secure FTP) pode ser o servidor FTP máis seguro para o mundo Linux/Unix, en canto a seguridade, rendemento e escalabilidade.\nSoporta o uso de certificados dixitais SSL, sempre e cando o cliente FTP os soporte (empregar o cliente FTP-SSL en lugar de FTP). Unha das peculiaridades que permite, e o uso de gaiolas chroot para os usuarios.\nUnha gaiola chroot, establece un directorio determinado como directorio raíz. Desde ese directorio, non se poderá acceder a ningún de nivel superior, pero si a todos os descendentes.\nPodemos atopar a súa web oficial no seguinte enlace: https://security.appspot.com/vsftpd.html\n",
+    "description": "",
+    "tags": null,
+    "title": "VsFTPD",
+    "uri": "/vsftpd/"
+  },
+  {
+    "content": "Para instalalo, en Linux, temos que instalar o paquete vsftpd\nsudo apt updatesudo apt install vsftpdEn la carpeta /etc encontramos la configuración del servidor ftp en el fichero /etc/vsftpd.conf voy a hacer una copia de seguridad para que no haya problemas en caso de error\nsudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bakEn este punto ya tenemos el servidor instalado y lo podemos arrancar como cualquier otro A continuación vamos a crear una cuenta de usuario para utilizar como cliente y acceder al servidor ftp.\nsudo useradd -m nombre-usuariosudo passwd nombre-usuario",
+    "description": "",
+    "tags": null,
+    "title": "1. Instalación",
+    "uri": "/vsftpd/instalacion/"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "2. Configuración",
+    "uri": "/vsftpd/configuracion/"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/"
+  },
+  {
+    "content": "No día de hoxe vamos a explicar o que o servicio FTP\nQue é o FTP? O FTP é un protocolo de transferencia de archivos, actualmente é o modelo estándar. Sobre este protocolo fóronse facendo sucesivas actualizacións para mellorar a súa funcionalidade. Estas actualizacións reciben outro nomes pero sempre conservando o FTP  \nActualizacions marcaraemos sobre que actaulizacion famos a tratar no dia de hoxe\n vsFTPd SFTP TFTP  ",
+    "description": "",
+    "tags": null,
+    "title": "UD5: Sistemas de ficheros web",
+    "uri": "/"
+  }
+]
